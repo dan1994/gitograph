73895a0164d14d5bdfc3372970aeb2a6a2d41602
Dan Arad <dan1994@gmail.com>
2021-04-10 13:48:35 +0300

    Move global styling into the MUI theme

--- src/renderer/app/Theme.ts
+++ src/renderer/app/Theme.ts
@@ -4,6 +4,23 @@ const theme = createMuiTheme({
     palette: {
         type: "dark",
     },
+    overrides: {
+        MuiCssBaseline: {
+            "@global": {
+                body: {
+                    fontFamily:
+                        '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+                    background: "#252526",
+                    color: "beige",
+                    display: "flex",
+                    justifyContent: "center",
+                },
+                "#root": {
+                    width: "100vw",
+                },
+            },
+        },
+    },
 });
 
 export default theme;
