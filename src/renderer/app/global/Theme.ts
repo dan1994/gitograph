b4ba029220f2de522f8a27ff0dc511720cf4c9c3
Dan Arad <dan1994@gmail.com>
2021-04-14 18:28:52 +0300

    Change font

--- src/renderer/app/global/Theme.ts
+++ src/renderer/app/global/Theme.ts
@@ -1,4 +1,5 @@
 import { createMuiTheme, Theme, ThemeOptions } from "@material-ui/core";
+import "renderer/static/fonts.css";
 
 interface IVscodeStyles {
     background: {
@@ -57,7 +58,7 @@ const theme = createMuiTheme({
             "@global": {
                 body: {
                     fontFamily:
-                        '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+                        'Rubik, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
                     backgroundColor: vscodeStyles.background.primary,
                     display: "flex",
                     justifyContent: "center",
