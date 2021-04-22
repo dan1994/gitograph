ccd183b7eefbf30f3ea73335676336120dd782a6
Dan Arad <dan1994@gmail.com>
2021-04-22 13:29:02 +0300

    `CommandRunner.run` is an async function now

--- src/renderer/app/utils/commandRunner/CommandRunner.ts
+++ src/renderer/app/utils/commandRunner/CommandRunner.ts
@@ -1,47 +1,19 @@
-import { CommandRunnerError } from "shared/commandRunner/customError";
-import { ChannelName } from "shared/ipc/channels";
 import { IpcRendererGuard } from "renderer/app/utils/ipc";
-
-type CommandCallback = (result: string) => void;
-
-interface Commands {
-    [command: string]: CommandCallback;
-}
+import { RendererIpcSuccessCallback } from "renderer/app/utils/ipc/IpcRendererGuard";
 
 class CommandRunner {
-    private static commands: Commands = {};
-
-    public static run: (
-        command: string,
-        successCallback: CommandCallback
-    ) => void = (command, callback) => {
-        CommandRunner.commands[command] = callback;
-        IpcRendererGuard.send("runCommand", command);
-    };
-
-    public static onSuccess: (command: string, result: string) => void = (
-        command,
-        result
-    ) => {
-        const callback = CommandRunner.commands[command];
-        delete CommandRunner.commands[command];
-        callback(result);
-    };
-
-    public static onFailure: (
-        error: CommandRunnerError,
-        channel: ChannelName
-    ) => void = (error, channel) => {
-        const command = error.command;
-        delete CommandRunner.commands[command];
-        IpcRendererGuard.defaultErrorHandler(error, channel);
+    public static run: (command: string) => Promise<string> = (command) => {
+        return new Promise<string>((resolve) => {
+            const successCallback: RendererIpcSuccessCallback<[string]> = (
+                results
+            ) => {
+                IpcRendererGuard.removeListener("runCommand", successCallback);
+                resolve(results);
+            };
+            IpcRendererGuard.on("runCommand", successCallback);
+            IpcRendererGuard.send("runCommand", command);
+        });
     };
 }
-
-IpcRendererGuard.on<[string, string]>(
-    "runCommand",
-    CommandRunner.onSuccess,
-    CommandRunner.onFailure
-);
 
 export default CommandRunner;
